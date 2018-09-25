64a44891e2c17edc6f2e2937087b762941e66582
Swegoverload456 <juanvelasquez983@gmail.com>
2018-09-25 07:44:55 -0400

    Update For The Main Processing code

--- DriveTrain.java
+++ DriveTrain.java
@@ -12,11 +12,11 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveTrain extends Subsystem{
 	
-	public AHRS Ahrs;
+	public TalonSRX frontLeft, frontRight, backLeft, backRight;
+	public AHRS ahrs;
 	
 	public Timer timer;
 	
@@ -28,14 +28,13 @@ public class DriveTrain extends Subsystem{
 	
 	public double CountsPerInch = ((CountsPerRev*GearReduction)/(WheelDiameter*Math.PI));
 	
-	TalonSRX frontLeft;
-	TalonSRX frontRight;
-	TalonSRX backLeft;
-	TalonSRX backRight;
-	
-	void setupNavx(AHRS ahrs) {
+	@Override
+	protected void initDefaultCommand() {
 		
-		ahrs = Ahrs;
+		frontLeft = new TalonSRX(0);
+		frontRight = new TalonSRX(1);
+		backLeft = new TalonSRX(2);
+		backRight = new TalonSRX(3);
 		
 		try {
 	          
@@ -48,96 +47,46 @@ public class DriveTrain extends Subsystem{
 	          
 	    }
 		
-	}
-	
-	@Override
-	protected void initDefaultCommand() {
-		
-		
-		
 		
 	}
 	
-	void setupTalon(TalonSRX Talon, boolean left, TalonSRX frontRight, boolean left2, TalonSRX backLeft, boolean left3, TalonSRX backRight, boolean left4 ) {
+	private void setupTalons(TalonSRX talon, boolean left) {
 		
-		frontLeft = Talon;
-		frontRight = frontRight;
-		backLeft = backLeft;
-		backRight = backRight;
-		
-		frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 5, 100);
-        final ErrorCode sensorPresent = frontLeft.configSelectedFeedbackSensor(FeedbackDevice
+		talon.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 5, 100);
+        final ErrorCode sensorPresent = talon.configSelectedFeedbackSensor(FeedbackDevice
                 .CTRE_MagEncoder_Relative, 0, 100); //primary closed-loop, 100 ms timeout
         if (sensorPresent != ErrorCode.OK) {
             DriverStation.reportError("Could not detect " + (left ? "left" : "right") + " encoder: " + sensorPresent, false);
         }
-        frontLeft.setInverted(!left);
-        frontLeft.setSensorPhase(true);
-        frontLeft.enableVoltageCompensation(true);
+        talon.setInverted(!left);
+        talon.setSensorPhase(true);
+        talon.enableVoltageCompensation(true);
         
-        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 5, 100);
-        final ErrorCode sensorPresent2 = frontRight.configSelectedFeedbackSensor(FeedbackDevice
-                .CTRE_MagEncoder_Relative, 0, 100); //primary closed-loop, 100 ms timeout
-        if (sensorPresent2 != ErrorCode.OK) {
-            DriverStation.reportError("Could not detect " + (left ? "left" : "right") + " encoder: " + sensorPresent2, false);
-        }
-        frontRight.setInverted(!left);
-        frontRight.setSensorPhase(true);
-        frontRight.enableVoltageCompensation(true);
 		
-        backLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 5, 100);
-        final ErrorCode sensorPresent3 = backLeft.configSelectedFeedbackSensor(FeedbackDevice
-                .CTRE_MagEncoder_Relative, 0, 100); //primary closed-loop, 100 ms timeout
-        if (sensorPresent3 != ErrorCode.OK) {
-            DriverStation.reportError("Could not detect " + (left ? "left" : "right") + " encoder: " + sensorPresent3, false);
-        }
-        backLeft.setInverted(!left);
-        backLeft.setSensorPhase(true);
-        backLeft.enableVoltageCompensation(true);
-        
-        backRight.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 5, 100);
-        final ErrorCode sensorPresent4 = backRight.configSelectedFeedbackSensor(FeedbackDevice
-                .CTRE_MagEncoder_Relative, 0, 100); //primary closed-loop, 100 ms timeout
-        if (sensorPresent4 != ErrorCode.OK) {
-            DriverStation.reportError("Could not detect " + (left ? "left" : "right") + " encoder: " + sensorPresent4, false);
-        }
-        backRight.setInverted(!left);
-        backRight.setSensorPhase(true);
-        backRight.enableVoltageCompensation(true);
-        
 	}
 	
-	double getEncoderValue(TalonSRX talon) {
+	private double getEncoderValue(TalonSRX talon) {
 		
 		return talon.getSelectedSensorPosition(0);
 		
 	}
 	
-	double getHeading() {
+	private double getHeading() {
 		
-		return Ahrs.getAngle();
-		
-	}
-	
-	void stopDrive(DriveTrain driveTrain) {
-		
-		driveTrain.frontLeft.set(ControlMode.PercentOutput, 0);
-		driveTrain.frontRight.set(ControlMode.PercentOutput, 0);
-		driveTrain.backLeft.set(ControlMode.PercentOutput, 0);
-		driveTrain.backRight.set(ControlMode.PercentOutput, 0);
+		return ahrs.getAngle();
 		
 	}
 	
-	void setPower(DriveTrain driveTrain, double flSpeed, double frSpeed, double blSpeed, double brSpeed) {
+	private void stopDrive() {
 		
-		driveTrain.frontLeft.set(ControlMode.PercentOutput, flSpeed);
-		driveTrain.frontRight.set(ControlMode.PercentOutput, frSpeed);
-		driveTrain.backLeft.set(ControlMode.PercentOutput, blSpeed);
-		driveTrain.backRight.set(ControlMode.PercentOutput, brSpeed);
+		frontLeft.set(ControlMode.PercentOutput, 0);
+		frontRight.set(ControlMode.PercentOutput, 0);
+		backLeft.set(ControlMode.PercentOutput, 0);
+		backRight.set(ControlMode.PercentOutput, 0);
 		
 	}
 	
-	void setTargetPos(TalonSRX talon, double target) {
+	private void setTargetPos(TalonSRX talon, double target) {
 		
 		while(true) {
 			
@@ -156,7 +105,7 @@ public class DriveTrain extends Subsystem{
 		
 	}
 	
-	/*public void drive(DriveTrain driveTrain, double inches, double timeout) {
+	public void drive(double inches, double timeout) {
 		
 		timer.reset();
 		timer.start();
@@ -180,7 +129,7 @@ public class DriveTrain extends Subsystem{
 			
 		}
 		
-		stopDrive(Robot.driveTrain);
+		stopDrive();
 		timer.stop();
 		
 	}
@@ -209,7 +158,7 @@ public class DriveTrain extends Subsystem{
 			
 		}
 		
-		stopDrive(Robot.driveTrain);
+		stopDrive();
 		timer.stop();
 		
 	}
@@ -258,20 +207,10 @@ public class DriveTrain extends Subsystem{
 					
 			}
 			
-			stopDrive(Robot.driveTrain);
+			stopDrive();
 			
 		}
+		
 	}
-	
-	public void getTelemetry() {
-		
-		SmartDashboard.putNumber("FrontLeft Encoder: ", getEncoderValue(Robot.driveTrain.frontLeft));
-		SmartDashboard.putNumber("FrontRight Encoder: ", getEncoderValue(Robot.driveTrain.frontRight));
-		SmartDashboard.putNumber("BackLeft Encoder: ", getEncoderValue(Robot.driveTrain.backLeft));
-		SmartDashboard.putNumber("BackRight Encoder: ", getEncoderValue(Robot.driveTrain.backRight));
-		
-		SmartDashboard.putNumber("Navx Heading: ", Ahrs.getAngle());
-		
-	}*/
 
 }
