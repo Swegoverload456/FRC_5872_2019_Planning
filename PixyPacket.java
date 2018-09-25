2f63d7d5af6334d4f6b2c2019df6c4093523808a
Swegoverload456 <juanvelasquez983@gmail.com>
2018-09-25 07:46:33 -0400

    Machine Code for Interpreting Pixy Values over I2C

---
+++ PixyPacket.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team5872.robot;
+
+public class PixyPacket {
+	double x, y;
+	double area;
+	
+	//for our use we only cared about a x, y, and area
+	//you could get any data you wanted
+}
